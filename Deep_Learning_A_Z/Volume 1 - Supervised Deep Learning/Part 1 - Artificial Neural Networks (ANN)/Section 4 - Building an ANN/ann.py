64e71d279d0d1a4a381e3f73a101fbb9557e59a0
xiaoyu zhou <xiaoyuz2011@gmail.com>
2018-04-13 18:16:46 -0400

    Add grid search for finding optimal hparameter

--- Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
+++ Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
@@ -30,36 +30,60 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+X_test = sc.transform(X_test)    
 
 # Implement ANN
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Dropout
 
-# Init ANN
-classifier = Sequential()
+# K fold validation
+from keras.wrappers.scikit_learn import KerasClassifier
 
-# Add input layer and 1st hidden layer
-classifier.add(Dense(activation="relu", input_dim=11, units=6, kernel_initializer="glorot_uniform"))
-# 2nd hidden layer
-classifier.add(Dense(activation="relu", units=6, kernel_initializer="glorot_uniform"))
-# Output layer
-classifier.add(Dense(activation="sigmoid", units=1, kernel_initializer="glorot_uniform"))
+def build_classifier(optimizer_in):
+    # Init ANN
+    classifier = Sequential()
+    # Add input layer and 1st hidden layer with dropout
+    classifier.add(Dense(activation="relu", input_dim=11, units=6, kernel_initializer="glorot_uniform"))
+    classifier.add(Dropout(rate=0.1))
+    # 2nd hidden layer with dropout
+    classifier.add(Dense(activation="relu", units=6, kernel_initializer="glorot_uniform"))
+    classifier.add(Dropout(rate=0.1))
+    # Output layer
+    classifier.add(Dense(activation="sigmoid", units=1, kernel_initializer="glorot_uniform"))
+    # Complie ANN
+    classifier.compile(optimizer=optimizer_in,loss='binary_crossentropy',metrics=['accuracy'])
+    return classifier
 
-# Complie ANN
-classifier.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+# Tuning hyper-parameter
+from sklearn.model_selection import GridSearchCV
+classifier = KerasClassifier(build_fn = build_classifier)
+hParameters = {'batch_size':[16,24,32,64],
+                'epochs':[50,100,250,500],
+                'optimizer_in':['adam','rmsprop']
+                }
+gridSearch = GridSearchCV(estimator = classifier,
+                          param_grid = hParameters,
+                          scoring='accuracy',
+                          cv=10)
 
-# Visual ANN
-from IPython.display import SVG
-from keras.utils.vis_utils import model_to_dot
-SVG(model_to_dot(classifier,show_shapes=True, show_layer_names=True, rankdir='TB').create(prog='dot', format='svg'))
+# Fit ANN to the training set with optimized hyper-parameters
+gridSearch = gridSearch.fit(X_train, y_train)
 
-from keras.utils import plot_model
-plot_model(classifier, to_file='model.png', show_shapes=True, show_layer_names=True, rankdir='LR')
+# Get best parameters and accuracy
+bestParams = gridSearch.best_params_
+bestAccuracy = gridSearch.best_score_
 
-# Fit ANN to the training set
-classifier.fit(X_train, y_train, batch_size=10, epochs=50)
+classifier = gridSearch.best_estimator_
+
+## Visual ANN
+#from IPython.display import SVG
+#from keras.utils.vis_utils import model_to_dot
+#SVG(model_to_dot(classifier,show_shapes=True, show_layer_names=True, rankdir='TB').create(prog='dot', format='svg'))
+#
+#from keras.utils import plot_model
+#plot_model(classifier, to_file='model.png', show_shapes=True, show_layer_names=True, rankdir='LR')
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
@@ -70,8 +94,6 @@ y_pred = (y_pred > 0.5)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
-
-
 
 nX = sc.transform(np.asarray([0,0,600,1,40,3,60000,2,1,1,50000]).reshape(1, -1))
 nX_pred = classifier.predict(nX)
