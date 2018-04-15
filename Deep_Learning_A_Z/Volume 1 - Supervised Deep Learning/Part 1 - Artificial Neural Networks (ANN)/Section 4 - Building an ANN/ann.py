82a5b225c442e80f124ef6db7e362ad260dcb71e
xiaoyu zhou <xiaoyuz2011@gmail.com>
2018-04-15 17:43:01 -0400

    adding grid search for finding optimal hparapmeter

--- Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
+++ Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
@@ -59,8 +59,8 @@ def build_classifier(optimizer_in):
 # Tuning hyper-parameter
 from sklearn.model_selection import GridSearchCV
 classifier = KerasClassifier(build_fn = build_classifier)
-hParameters = {'batch_size':[16,24,32,64],
-                'epochs':[50,100,250,500],
+hParameters = {'batch_size':[4,16,32,64,128],
+                'epochs':[50,100,250,360,480,520],
                 'optimizer_in':['adam','rmsprop']
                 }
 gridSearch = GridSearchCV(estimator = classifier,
@@ -75,7 +75,9 @@ gridSearch = gridSearch.fit(X_train, y_train)
 bestParams = gridSearch.best_params_
 bestAccuracy = gridSearch.best_score_
 
-classifier = gridSearch.best_estimator_
+###
+classifier.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+classifier.fit(X_train, y_train, batch_size=10, epochs=50)
 
 ## Visual ANN
 #from IPython.display import SVG
@@ -94,6 +96,42 @@ y_pred = (y_pred > 0.5)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Classifier (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Classifier (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show() 
 
 nX = sc.transform(np.asarray([0,0,600,1,40,3,60000,2,1,1,50000]).reshape(1, -1))
 nX_pred = classifier.predict(nX)
